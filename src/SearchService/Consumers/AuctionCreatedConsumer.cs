ecb0ca27f32c9ae6fceb47ca5ccf2f30f794b48a
Szabolcs Peter <szabi.peter@gmail.com>
2024-10-11 15:53:41 +0200

    consuming faulty queues

--- src/SearchService/Consumers/AuctionCreatedConsumer.cs
+++ src/SearchService/Consumers/AuctionCreatedConsumer.cs
@@ -20,6 +20,8 @@ namespace SearchService.Consumers
 
             var item = _mapper.Map<Item>(context.Message);
 
+            if (item.Model == "Foo") throw new ArgumentException("Cannot sell cars with name of Foo");
+
             await item.SaveAsync();
         }
     }
